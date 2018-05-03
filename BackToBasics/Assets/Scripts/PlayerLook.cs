f29db7fcbdc0e6c11b4df0300667f6d3809654b9
Steve Sparkes <contact@stevesparkes.com>
2018-05-03 22:37:12 +0100

    Created PlayerLook script
    Created PlayerLook script.

---
+++ BackToBasics/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour {
+
+    float mouseX;
+    float mouseY;
+    public float mouseSensitivity;
+    float rotAmount_X;
+    float rotAmount_Y;
+    Vector3 targetRot;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+
+        rotAmount_X = mouseX * mouseSensitivity;
+        rotAmount_Y = mouseY * mouseSensitivity;
+
+        targetRot = transform.rotation.eulerAngles;
+
+        targetRot.x += rotAmount_Y;
+        targetRot.y += rotAmount_X;
+
+        transform.rotation = Quaternion.Euler(targetRot);
+	}
+}
