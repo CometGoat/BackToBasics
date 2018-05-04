123aee183cee637f9516897961860679bc4107be
Steve Sparkes <contact@stevesparkes.com>
2018-05-04 23:57:07 +0100

    Attempting to implement jump code
    Attempting to implement jump code.
    
    Co-Authored-By: Magdalena Stankova <17054923@users.noreply.github.com>

--- BackToBasics/Assets/Scripts/PlayerMove.cs
+++ BackToBasics/Assets/Scripts/PlayerMove.cs
@@ -6,18 +6,40 @@ public class PlayerMove : MonoBehaviour {
 
     CharacterController charControl;
     public float walkSpeed;
+    public float jumpStrength = 1.0f;
+    public Rigidbody boob; 
 
 	// Use this for initialization
 	void Start ()
     {
         charControl = GetComponent<CharacterController>();
+        boob = GetComponent<Rigidbody>();
 	}
+
+    void FixedUpdate()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //boob.velocity = new Vector3(0, 10 * jumpStrength, 0);
+            boob.AddForce(new Vector3(100, 100, 100), ForceMode.Impulse);
+            print("splooble booble");
+        }
+    }
+
 	
 	// Update is called once per frame
 	void Update ()
     {
-        MovePlayer();
-	}
+         MovePlayer();
+
+        //if (Input.GetKeyDown("space"))
+        //{
+        //    boob.velocity = new Vector3(0, 10 * jumpStrength, 0);
+        //    print("splooble booble");
+        //}
+
+    }
 
     void MovePlayer()
     {
