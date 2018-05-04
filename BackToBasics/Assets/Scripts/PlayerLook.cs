edbdfe0e625c38cb5c6d173ff0fba2b188f1c0a0
Steve Sparkes <contact@stevesparkes.com>
2018-05-04 01:14:28 +0100

    Created PlayerMove script
    Created PlayerMove script.

--- BackToBasics/Assets/Scripts/PlayerLook.cs
+++ BackToBasics/Assets/Scripts/PlayerLook.cs
@@ -11,6 +11,7 @@ public class PlayerLook : MonoBehaviour {
     float rotAmount_Y;
     Vector3 targetRot_Cam;
     Vector3 targetRot_Body;
+    float xAxisClamp = 0.0f;
 
     public Transform playerBody;
 
@@ -29,11 +30,24 @@ public class PlayerLook : MonoBehaviour {
         rotAmount_X = mouseX * mouseSensitivity;
         rotAmount_Y = mouseY * mouseSensitivity;
 
+        xAxisClamp -= rotAmount_Y;
+
         targetRot_Cam = transform.rotation.eulerAngles;
         targetRot_Body = playerBody.rotation.eulerAngles;
 
         targetRot_Cam.x -= rotAmount_Y;
         targetRot_Body.y += rotAmount_X;
+
+        if (xAxisClamp > 90)
+        {
+            xAxisClamp = 90;
+            targetRot_Cam.x = 90;
+        }
+        else if(xAxisClamp < -90)
+        {
+            xAxisClamp = -90;
+            targetRot_Cam.x = 270;
+        }
 
         transform.rotation = Quaternion.Euler(targetRot_Cam);
         playerBody.rotation = Quaternion.Euler(targetRot_Body);
